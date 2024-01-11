5f39a4722d8511ae5bdff609e41b11fef05509e6
nad1408 <155629463+nad1408@users.noreply.github.com>
2024-01-11 09:31:43 +0000

    true or false ++

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -1,19 +1,50 @@
 // Entscheidungsstrukturen | control flow
 
 // Deklaration + Assignment
-const ageJohn = 32;
+const ageJohn = 30;
 const ageMark = 30;
 
 // Deklaration
-let isJohnOlder, isJohnEqual;
+let isJohnOlder, isJohnEqual; ismarkyounger;
 
 // Test | Logische Aussage
 isJohnOlder = (ageJohn > ageMark);
 isJohnEqual = (ageJohn == ageMark);
 
-// Ausgabe
-// console.log("ageJohn: " + ageJohn);
-// console.log("ageMark: " + ageMark);
-// console.log("isJohnOlder: " + isJohnOlder);
-// console.log("isJohnEqual: " + isJohnEqual);
-// console.log("------------------");
+//Ausgabe
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
+console.log("------------------");
+
+/************ IF  ************/
+// TINA --> There is no alternative!
+// entweder JA oder nix ... alternativlos
+// Türsteher-Politik (Doors 10 p.m.)
+
+
+
+//if(true)
+//if(false)
+if(isJohnOlder) // ohne semikolon wird die bedingung nur ausgeführt wenn  es zutrifft!
+{
+    console.log("john is älter")  
+}
+if(isJohnEqual)
+{
+    console.log("john is gleich alter")  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
