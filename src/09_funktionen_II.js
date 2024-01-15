d3507d3d6ee936eaea2c98271ed200a0ef8bf4e6
nad1408 <155629463+nad1408@users.noreply.github.com>
2024-01-15 09:50:11 +0000

    09/divide++ var. 1

--- src/09_funktionen_II.js
+++ src/09_funktionen_II.js
@@ -10,10 +10,24 @@
 4. Ausgabe in Konsole : check
 */
 
+// module: division a / b |  test:
+output(divide(4,2));
+output(divide(3,2));
+output(divide(3,-2));
+output(divide(0,2));
+output(divide(3,0));
+function divide(a,b) {
+	if (b !== 0) {
+		return a / b;
+	} else {
+		return "Division durch Null ist nicht definiert";
+	}
+}
+
 // module: multiply a - b |  test:
-output(multiply(3,2));
-output(multiply(3,-2));
-output(multiply(3,0));
+// output(multiply(3,2));
+// output(multiply(3,-2));
+// output(multiply(3,0));
 function multiply(a,b) {
 	return a * b;
 }
@@ -23,7 +37,7 @@ function multiply(a,b) {
 // output(subtract(3,2))
 // output(subtract(3,-2))
 // output(subtract(3,0))
-// output(subtract(0,2))
+
 function subtract(a,b) {
 	return a - b ;
 }
