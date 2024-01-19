5670472c160347c6a8a6d6dde1ff56828e99f006
nad1408 <155629463+nad1408@users.noreply.github.com>
2024-01-19 11:15:28 +0000

    updates

--- src/14_objects.js
+++ src/14_objects.js
@@ -1,42 +1,90 @@
-/***** Erinnerung: Arrays *******/
+// /***** Erinnerung: Arrays *******/
 
 
 
-let arr;
-arr = ["Ich","bin","Max"];
-output(arr);
-output(arr[0]);     // das erste element aus dem array
-output("------------");
+// let arr;
+// arr = ["Ich","bin","Max"];
+// output(arr);
+// output(arr[0]);     // das erste element aus dem array
+// output("------------");
 
-// // nested arrays - Prinzip: [[[]]]
-// // nested arrays  - hier: [[],[]]
-arr =   [
-                ["Ich","bin","Max"],
-                ["Ich","bin","Moritz"]
-        ];
+// // // nested arrays - Prinzip: [[[]]]
+// // // nested arrays  - hier: [[],[]]
+// arr =   [
+//                 ["Ich","bin","Max"],
+//                 ["Ich","bin","Moritz"]
+//         ];
  
         
-output(arr);
-output(arr[0]);
-output(arr[0][0]+ " " + arr[1][2]);
-output(arr[1][0]);
-output("------------");
+// output(arr);
+// output(arr[0]);
+// output(arr[0][0]+ " " + arr[1][2]);
+// output(arr[1][0]);
+// output("------------");
 
-// nested loop
+// // nested loop
 
-for (let i = 0; i < arr.length; i++) {
+// for (let i = 0; i < arr.length; i++) {
 
-    for (let j = 0; j < arr[i].length; j++) {
+//     for (let j = 0; j < arr[i].length; j++) {
 
-        output("Index i: " + i + " Index j: " + j);
+//         output("Index i: " + i + " Index j: " + j);
 
-        output(arr[i][j]);      
+//         output(arr[i][j]);      
 
+// }
+
+
+let person = {
+    firstName:"Behrang",
+    familyName:"Valadkhani",
+    salary: [120000, 160000],
+    permission: true,
+    sayHello: function(){                         // funkionn ohne namen
+        // return "Hello, ich bin" + person.firstName;
+        return "Hello, ich bin" + person.firstName;
     }
+};
 
-}
+
+// output(person);
+//output(person.familyName)
+// output(person.firstName)
+// output(person.salary[1])
+// output(person.permission)
+// output(person.sayHello())
+
+const txt = person.sayHello() + ", ich bin " + person.firstName +
+" und verdiene " + person.salary[1] + " p.a."
+// output(txt);
+
+
+
+/***** Objekte 2 - Hierarchie *******/
+// Tiefer, tiefer - Irgendwo in der Tiefe gibt es ein Licht // K.Bush
+// Der Baikalsee ist mit 1642 m der tiefste See der Erde. ...
+// deep - deeper - deepest
+// nested object {{{}}}
+
+let baikal = {
+            value: "10m",
+            deep: {
+                deeper:{
+                  temperature:"4 Grad",
+                  deepest: "Das Licht - auf 1642m!"  
+                }
+            }
+        };
+
+output(baikal);            
+output(baikal.value);
+output(baikal.deep);
+output(baikal.deep.deeper.deepest);
+
+            
+
 // Modul: Ausgabe | Test
 //output("hi");
 function output(outputData) {
-    console.log(outputData);
+console.log(outputData);
 }
